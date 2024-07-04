65fcf2b0f5572ad43f16c49e3165b158d710a603
Leo Diaz Sanchez <leodiazsanchez01@gmail.com>
2024-07-04 17:58:14 +0200

    started work on backend

--- src/scripts/authCodeWithPkce.ts
+++ src/scripts/authCodeWithPkce.ts
@@ -1,98 +1,122 @@
 export const clientId = "e5f9bfa9d40447488e4fc74d2c71d293";
 
-
-
 export async function redirectToAuthCodeFlow(clientId: string) {
-    const verifier = generateCodeVerifier(128);
-    const challenge = await generateCodeChallenge(verifier);
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
 
-    localStorage.setItem("verifier", verifier);
+  localStorage.setItem("verifier", verifier);
 
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:3000/callback");
-    params.append("scope", "user-read-private user-read-email user-top-read user-read-playback-state user-modify-playback-state");
-    params.append("code_challenge_method", "S256");
-    params.append("code_challenge", challenge);
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "http://localhost:3000/callback");
+  params.append(
+    "scope",
+    "user-read-private user-read-email user-top-read user-read-playback-state user-modify-playback-state"
+  );
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
 
-    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
 }
 
 export async function getAccessToken(clientId: string, code: string) {
-    const verifier = localStorage.getItem("verifier");
+  const verifier = localStorage.getItem("verifier");
 
-    const params = new URLSearchParams();
-    params.append("client_id", clientId);
-    params.append("grant_type", "authorization_code");
-    params.append("code", code);
-    params.append("redirect_uri", "http://localhost:3000/callback");
-    params.append("code_verifier", verifier!);
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("grant_type", "authorization_code");
+  params.append("code", code);
+  params.append("redirect_uri", "http://localhost:3000/callback");
+  params.append("code_verifier", verifier!);
 
-    const result = await fetch("https://accounts.spotify.com/api/token", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: params
-    });
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params,
+  });
 
-    const { access_token } = await result.json();
-    return access_token;
+  const { access_token } = await result.json();
+  return access_token;
 }
 
 function generateCodeVerifier(length: number) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
 
 async function generateCodeChallenge(codeVerifier: string) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
 }
 
-export function isAccessTokenExpired(accessToken: string, expirationTimeInSeconds: number = 3600): boolean {
-    if (!accessToken) {
-        console.log("token" + accessToken)
-        return true; // If accessToken is not provided, consider it expired
+const getRefreshToken = async () => {
+  try {
+    // Refresh token that has been previously stored
+    const refreshToken = localStorage.getItem("refresh_token");
+    const url = "https://accounts.spotify.com/api/token";
+
+    if (!refreshToken) {
+      throw new Error("No refresh token available.");
     }
 
-    const issuedAt = new Date(localStorage.getItem("accessTokenTimestamp")).getSeconds();
+    const payload = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: refreshToken,
+        client_id: clientId,
+      }),
+    };
 
-    const now = new Date().getSeconds();
+    const response = await fetch(url, payload);
 
-    
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
 
-    return now > issuedAt + expirationTimeInSeconds;
-}
+    const data = await response.json();
+
+    localStorage.setItem("access_token", data.access_token);
+    localStorage.setItem("refresh_token", data.refresh_token);
+  } catch (error) {
+    console.error("Failed to refresh token", error);
+  }
+};
 
 export interface UserProfile {
-    country: string;
-    display_name: string;
-    email: string;
-    explicit_content: {
-        filter_enabled: boolean,
-        filter_locked: boolean
-    },
-    external_urls: { spotify: string; };
-    followers: { href: string; total: number; };
-    href: string;
-    id: string;
-    images: Image[];
-    product: string;
-    type: string;
-    uri: string;
+  country: string;
+  display_name: string;
+  email: string;
+  explicit_content: {
+    filter_enabled: boolean;
+    filter_locked: boolean;
+  };
+  external_urls: { spotify: string };
+  followers: { href: string; total: number };
+  href: string;
+  id: string;
+  images: Image[];
+  product: string;
+  type: string;
+  uri: string;
 }
 
 interface Image {
-    url: string;
-    height: number;
-    width: number;
+  url: string;
+  height: number;
+  width: number;
 }
